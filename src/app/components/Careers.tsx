a3f073e3a8b5954a392e4ae22610ab5a144f72fd
rowan.ibrahim <rowan.ibrahim@yomicepa.com>
2021-04-15 22:22:06 +0200

    seperate career form from contacts section

--- src/app/components/Careers.tsx
+++ src/app/components/Careers.tsx
@@ -1,58 +1,199 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import Navigation from './navigation';
 import { Header } from './header';
 import { Footer } from './Footer';
-interface IProps extends RouteComponentProps{}
-export const Careers:FunctionComponent<IProps>=()=>{
-    return(
-        <div>
-        <div id="careers">
-          <div className="container">
-        <form name="sentMessage" id="contactForm" noValidate>
-            <div className="row">
-            <div className="col-md-6">
-                <div className="form-group">
-                <input
-                    type="text"
-                    id="name"
-                    className="form-control"
-                    placeholder="Name"
-                    required
-                />
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            <div className="col-md-6">
-                <div className="form-group">
-                <input
-                    type="email"
-                    id="email"
-                    className="form-control"
-                    placeholder="Email"
-                    required
-                />
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            </div>
-            <div className="form-group">
-            <textarea
-                name="message"
-                id="message"
-                className="form-control"
-                rows={4}
-                placeholder="Message"
-                required
-            ></textarea>
-            <p className="help-block text-danger"></p>
-            </div>
-            <div id="success"></div>
-            <button type="submit" className="btn btn-custom btn-lg">
-            Apply now
-            </button>
-        </form>
-        </div></div>
-        </div>
-    )
+import { FaCheck, FaUpload, FaTrash } from 'react-icons/fa';
+import { EMartialStatus, EMilitaryStatus, EGender } from '../api/Interfaces';
+interface IProps extends RouteComponentProps {
+	onSubmit: any;
+	submissionMessage?: string;
+	setFileUploaded: any;
+	errorValidation: any;
+	onClose:any
+}
+export const Careers: FunctionComponent<IProps> = (props: IProps) => {
+	const [fileUploaded, setFileUploaded] = useState<any>(undefined);
+	const onUpload = (event: any) => {
+		var file = event.target.files[0];
+		event.target.value = null;
+		setFileUploaded(file);
+		props.setFileUploaded(file);
+	}
+	const onRemove = () => {
+		setFileUploaded(undefined);
+	}
+	const renderSubmittionMessage = () => {
+		return (
+			<div className="row text-center" >
+				<FaCheck color="green" style={{ width: '5em', height: '5em' }} />
+				<h3 >
+					{props.submissionMessage}
+
+				</h3>
+			</div>
+		)
+	}
+	const modalBody = () => {
+		return (
+			<div className="contact-info">
+				<h5 style={{ color: "black" }}> Please fill your data. All field are required!</h5>
+				{props.errorValidation && <small style={{ color: 'red' }}>Please enter all fields.</small>}
+				<form onSubmit={props.onSubmit} name="sentMessage" id="contactForm" noValidate>
+					<div className="row">
+						<div className="col-md-6">
+							<div className="form-group">
+								<input
+									type="text"
+									id="name"
+									className="form-control"
+									placeholder="Name"
+									required
+								/>
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+						<div className="col-md-6">
+							<div className="form-group">
+								<input
+									type="email"
+									id="email"
+									className="form-control"
+									placeholder="Email"
+									required
+								/>
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-6">
+							<div className="form-group">
+								<input
+									type="tel"
+									id="phone"
+									className="form-control"
+									placeholder="Mobile no."
+									required
+								/>
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+						<div className="col-md-6">
+							<div className="form-group">
+								<input
+									type="date"
+									id="date"
+									className="form-control"
+									placeholder="Date of Birth"
+									required
+								/>
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+
+					</div>
+					<div className="row">
+						<div className="col-md-4">
+							<div className="form-group">
+								<select
+									id="gender"
+									className="form-control"
+									placeholder="Gender"
+									required
+								>
+									<option value="">Gender</option>
+									<option value={EGender.female}>Female</option>
+									<option value={EGender.male}>Male</option>
+								</select >
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="form-group">
+								<select
+									id="martialStatus"
+									className="form-control"
+									placeholder="Martial Status"
+									required
+								>
+									<option value="">Martial Status</option>
+									<option value={EMartialStatus.single}>Single</option>
+									<option value={EMartialStatus.married}>Married</option>
+									<option value={EMartialStatus.divorced}>Divorced</option>
+									<option value={EMartialStatus.widowed}>Widowed</option>
+									<option value={EMartialStatus.seperated}>Separated</option>
+								</select>
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="form-group">
+								<select
+									id="militaryStatus"
+									className="form-control"
+									placeholder="Military Status"
+									required
+								>
+									<option value="">Military Status</option>
+									<option value={EMilitaryStatus.completed}>Completed</option>
+									<option value={EMilitaryStatus.postponed}>Postponed</option>
+									<option value={EMilitaryStatus.notApplicable}>Not Applicable</option>
+									<option value={EMilitaryStatus.exempted}>Exempted</option>
+								</select>
+								<p className="help-block text-danger"></p>
+							</div>
+						</div>
+
+					</div>
+					<div className="row">
+						<div className="col-md-6">
+							{/* <div className="form-group"> */}
+							<input type="file" id="file" onChange={(e) => { onUpload(e) }} accept=".pdf,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" />
+							<label htmlFor="file" className="btn-2"><FaUpload />  Upload</label>
+							{/* <input type="file" id="docpicker"
+                      accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" /> */}
+							<p className="help-block text-danger"></p>
+							{/* </div> */}
+
+						</div>
+						{fileUploaded && <div className="col-md-6" style={{ padding: "1rem 50px", color: "black", maxWidth: '100%' }}>
+							{fileUploaded.name} <FaTrash onClick={onRemove} />
+						</div>}
+					</div>
+					<button type="submit" className="btn btn-custom-form btn-lg" style={{ marginLeft: '35%', marginRight: '65%', padding: "1rem 50px" }}>
+						Submit
+                    </button>
+					<div id="success"></div>
+
+				</form>
+			</div>
+		)
+	}
+	const onClose=()=>{
+		Array.from(document.querySelectorAll("input")).forEach(
+      input => (input.value = "")
+    );
+		props.onClose();
+	}
+	return (
+		<div className="modal fade" id="careers" role="dialog">
+			<div className="modal-dialog">
+
+				<div className="modal-content">
+					<div className="modal-header">
+						<button type="button" className="close" data-dismiss="modal" onClick={onClose}>&times;</button>
+						<h4 className="modal-title">Join Us!</h4>
+					</div>
+					<div className="modal-body" style={{ marginTop: '20px', marginLeft: '20px', marginRight: '20px' }}>
+						{(!props.submissionMessage) ? modalBody() : renderSubmittionMessage()}
+					</div>
+					<div className="modal-footer">
+						<button type="button" className="btn btn-danger" data-dismiss="modal" onClick={props.onClose}>Close</button>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	)
 }
--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -5,6 +5,7 @@ import { FaUpload, FaTrash, FaCheck } from "react-icons/fa";
 import _ from 'lodash';
 import { EMartialStatus, EMilitaryStatus, EGender, ICareersBody, IContactsBody } from "../api/Interfaces";
 import { apply } from "../api/Api";
+import { Careers } from "./Careers";
 
 var Slide =require('react-reveal/Slide');
 interface IProps{
@@ -39,8 +40,8 @@ interface ICareerData{
 }
 export const Contact =(props:IProps)=> {
   const [fileUploaded, setFileUploaded] = useState<any>(undefined);
-  const [submissionMessage, setSubmissionMessage] = useState<string>();
   const [errorMessage, setErrorMessage] = useState<boolean>(false);
+  const [submissionMessage, setSubmissionMessage] = useState<string>();
 
   const checkForMissingData=(target:ICareerData)=>{
     if(_.isEmpty(target.email.value)
@@ -79,163 +80,12 @@ export const Contact =(props:IProps)=> {
     else{
       setErrorMessage(true);
     }
-  }
-
-  const onUpload=(event:any)=>{
-    var file = event.target.files[0];
-    event.target.value = null;
-    setFileUploaded(file);
-
-  }
-  const onRemove=()=>{
+  }  
+  const onCareerClosed=()=>{
+    setErrorMessage(false);
+    setSubmissionMessage(undefined);
     setFileUploaded(undefined);
-  }
-  const renderSubmittionMessage=()=>{
-    return(
-      <div className="row text-center" >
-      <FaCheck color="green" style={{width:'5em', height:'5em'}}/>
-      <h3 >
-        {submissionMessage}
-        
-      </h3>
-      </div>
-    )
-  }
-  const modalBody=()=>{
-    return(
-      <div className="contact-info">
-       <h5 style={{color:"black"}}> Please fill your data. All field are required!</h5>
-       {errorMessage && <small style={{color:'red'}}>Please enter all fields.</small>}
-      <form onSubmit={onSubmit} name="sentMessage" id="contactForm" noValidate>
-            <div className="row">
-            <div className="col-md-6">
-                <div className="form-group">
-                <input
-                    type="text"
-                    id="name"
-                    className="form-control"
-                    placeholder="Name"
-                    required
-                />
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            <div className="col-md-6">
-                <div className="form-group">
-                <input
-                    type="email"
-                    id="email"
-                    className="form-control"
-                    placeholder="Email"
-                    required
-                />
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            </div>
-            <div className="row">
-            <div className="col-md-6">
-                <div className="form-group">
-                <input
-                    type="tel"
-                    id="phone"
-                    className="form-control"
-                    placeholder="Mobile no."
-                    required
-                />
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            <div className="col-md-6">
-                <div className="form-group">
-                <input
-                    type="date"
-                    id="date"
-                    className="form-control"
-                    placeholder="Date of Birth"
-                    required
-                />
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            
-            </div>
-            <div className="row">
-            <div className="col-md-4">
-                <div className="form-group">
-                <select 
-                    id="gender"
-                    className="form-control"
-                    placeholder="Gender"
-                    required
-                >
-                  <option value="">Gender</option>
-                  <option value={EGender.female}>Female</option>
-                  <option value={EGender.male}>Male</option>
-                  </select >
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="form-group">
-                <select
-                    id="martialStatus"
-                    className="form-control"
-                    placeholder="Martial Status"
-                    required
-                >
-                  <option value="">Martial Status</option>
-                  <option value={EMartialStatus.single}>Single</option>
-                  <option value={EMartialStatus.married}>Married</option>
-                  <option value={EMartialStatus.divorced}>Divorced</option>
-                  <option value={EMartialStatus.widowed}>Widowed</option>
-                  <option value={EMartialStatus.seperated}>Separated</option>
-                  </select>
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="form-group">
-                <select
-                    id="militaryStatus"
-                    className="form-control"
-                    placeholder="Military Status"
-                    required
-                >
-                  <option value="">Military Status</option>
-                  <option value={EMilitaryStatus.completed}>Completed</option>
-                  <option value={EMilitaryStatus.postponed}>Postponed</option>
-                  <option value={EMilitaryStatus.notApplicable}>Not Applicable</option>
-                  <option value={EMilitaryStatus.exempted}>Exempted</option>
-                  </select>
-                <p className="help-block text-danger"></p>
-                </div>
-            </div>
-            
-            </div>
-            <div className="row">
-            <div className="col-md-6">
-                {/* <div className="form-group"> */}
-                <input type="file" id="file" onChange={(e)=>{onUpload(e)}} accept=".pdf,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"/>
-                <label htmlFor="file" className="btn-2"><FaUpload/>  Upload</label>
-                {/* <input type="file" id="docpicker"
-                  accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" /> */}
-                <p className="help-block text-danger"></p>
-                {/* </div> */}
-                
-            </div>
-            {fileUploaded && <div className="col-md-6" style={{padding: "1rem 50px", color:"black", maxWidth:'100%'}}>
-                {fileUploaded.name} <FaTrash onClick={onRemove} />
-            </div>}
-            </div>
-            <button type="submit" className="btn btn-custom-form btn-lg" style={{marginLeft:'35%', marginRight:'65%', padding: "1rem 50px"}}>
-                  Submit
-                </button>
-            <div id="success"></div>
-            
-        </form>
-        </div>
-    )
+    setFileUploaded(undefined);
   }
     return (
       <div>
@@ -255,24 +105,8 @@ export const Contact =(props:IProps)=> {
                   Apply now
                 </button>
                 
-                <div className="modal fade" id="careers" role="dialog">
-                  <div className="modal-dialog">
-                  
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal">&times;</button>
-                        <h4 className="modal-title">Join Us!</h4>
-                      </div>
-                      <div className="modal-body" style={{marginTop:'20px', marginLeft:'20px', marginRight:'20px'}}>
-                        {(!submissionMessage) ? modalBody() : renderSubmittionMessage()}
-                      </div>
-                      <div className="modal-footer">
-                        <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
-                      </div>
-                    </div>
-                    
-                  </div>
-                </div>
+                <Careers onSubmit={onSubmit} setFileUploaded={setFileUploaded} onClose={onCareerClosed}
+                errorValidation={errorMessage} submissionMessage={submissionMessage}/>
                 
               </div>
               </div>
