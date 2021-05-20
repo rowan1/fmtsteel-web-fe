78147c61e733a8000a69bb7b55214b2bee6708af
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2021-05-21 01:58:50 +0200

    refactor services

--- src/app/admin/components/ServiceModal.tsx
+++ src/app/admin/components/ServiceModal.tsx
@@ -1,71 +1,106 @@
-import React, { useState } from 'react';
-import { Modal } from '../../shared/modal/Modal';
-import { ModalHeader } from '../../shared/modal/ModalHeader';
-import { ModalBody } from '../../shared/modal/ModalBody';
-import { ModalFooter } from '../../shared/modal/ModalFooter';
+import React, { useState, useEffect } from 'react'
+import { Button, Header, Image, Modal, Grid, Checkbox, Form } from 'semantic-ui-react';
 import { IServicesBody } from '../../api/Interfaces';
 
-interface IProp{
-	service?:IServicesBody,
-	onAdd?:any
+interface IProps{
+    open:boolean,
+    onAction: any,
+    service?:IServicesBody,
+    onSubmit?:any
 }
-export const ServiceModal = (props:IProp) => {
-	const[service, setService] = useState<IServicesBody>({});
-	console.log(props.service?.title);
-	const onSubmit=()=>{
-		props.onAdd(service)
-	}
-	const modalBody = () => {
-		return (
-			<div className="container">
-				<form data-autocomplete="off" onSubmit={onSubmit} name="updateContact" id="contactForm" noValidate>
-					<div className="row">
-						<div className="col-md-6">
-							<div className="form-group">
-								<input
-									type="name"
-									id="name"
-									className="form-control"
-									placeholder="Service Name"
-									required
-									defaultValue={props.service?.title}
-									onChange={(e:any)=>{setService({...service,title:e.target.value})}}
-								/>
-								<p className="help-block text-danger"></p>
-							</div>
-						</div>
-
-					</div>
-					<div className="row">
-						<div className="col-md-6">
-							<div className="form-group">
-								<textarea
-									name="description"
-									id="description"
-									className="form-control"
-									rows={2}
-									placeholder="Description"
-									required
-									onChange={(e:any)=>{setService({...service,description:e.target.value})}}
-									defaultValue={props?.service?.description}
-								></textarea>
-								<p className="help-block text-danger"></p>
-							</div>
-						</div>
-					</div>
-					<button onClick={(e:any)=>{e.preventDefault(); onSubmit()}} type="submit" className="btn btn-primary"  data-dismiss="modal">Save</button>
-					<div id="success"></div>					
-				</form>
-			</div>
-		)
-	}
-	return (
-		<Modal
-			header={<ModalHeader title="Service Details" />}
-			body={<ModalBody bodyElements={modalBody()} />}
-			footer={<ModalFooter footerElements={<>
-			<button type="button" className="btn btn-danger" data-dismiss="modal">Cancel</button></>
-			} />}
-		/>
-	)
+export const ServiceModal=(props:IProps)=> {
+  const [currentService, setCurrentService] = useState<IServicesBody>();
+  const closeOnEscape = true
+  const closeOnDimmerClick = true
+  const dimmer = undefined
+  useEffect(()=>{
+    props.service && setCurrentService(props.service);
+  },[])
+//   return (
+//     <Modal
+//       height={'500'}
+//       onClose={() => setOpen(false)}
+//       onOpen={() => setOpen(true)}
+//       open={open}
+//       closeOnEscape={closeOnEscape}
+//       closeOnDimmerClick={closeOnDimmerClick}
+//       size={"small"}
+//       trigger={<Button className="btn btn-custom btn-lg" style={{ 
+//         fontFamily: 'Raleway',
+//         textTransform: 'uppercase',
+//         color: '#fff',
+//         backgroundColor: '#c95f44',
+//         backgroundImage: 'linear-gradient(to right, #db3630 0%, #c95f44 100%)',
+//         padding: '14px 34px',
+//         letterSpacing: '1px',
+//         margin: '10',
+//         fontSize: '15px',
+//         fontWeight: '500',
+//         borderRadius: '25px',
+//         transition: 'all 0.5s linear',
+//         border: '0'
+//      }}>Add new Service</Button>}
+//     >
+//       <Modal.Header>Select a Photo</Modal.Header>
+//       <Modal.Content image>
+//         <Modal.Description>
+//           <Header>Default Profile Image</Header>
+//           <p>
+//             We've found the following gravatar image associated with your e-mail
+//             address.
+//           </p>
+//           <p>Is it okay to use this photo?</p>
+//         </Modal.Description>
+//       </Modal.Content>
+//       <Modal.Actions>
+//         <Button color='black' onClick={() => setOpen(false)}>
+//           Nope
+//         </Button>
+//         <Button
+//           content="Yep, that's me"
+//           labelPosition='right'
+//           icon='checkmark'
+//           onClick={() => setOpen(false)}
+//           positive
+//         />
+//       </Modal.Actions>
+//     </Modal>
+//   )
+return (
+    <Grid columns={1}>
+      <Grid.Column>
+        <Modal
+          closeOnEscape={closeOnEscape}
+          closeOnDimmerClick={closeOnDimmerClick}
+          open={props.open}
+          onClose={() => props.onAction(false)}
+          onOpen={() => props.onAction(true)}
+        //   trigger={<Button >Show Modal</Button>}
+        >
+          <Modal.Header>Service Details</Modal.Header>
+          <Modal.Content>
+          <Form>
+            <Form.Field>
+            <label>Service Name</label>
+            <input placeholder='Service Name' defaultValue={props.service?.title} 
+            onChange={(e)=>{setCurrentService({...currentService,title:e.target.value})}}/>
+            </Form.Field>
+            <Form.Field>
+            <label>Description</label>
+            <input placeholder='Description' defaultValue={props.service?.description}
+            onChange={(e)=>{setCurrentService({...currentService,description:e.target.value})}}/>
+            </Form.Field>
+            
+            <Button type='submit' onClick={()=>{props.onSubmit(currentService)}}>Submit</Button>
+        </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button onClick={() => props.onAction(false)} negative>
+              Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </Grid.Column>
+    </Grid>
+  )
 }
--- src/app/admin/pages/Services.tsx
+++ src/app/admin/pages/Services.tsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
-import { FaEdit, FaTrashAlt } from 'react-icons/fa';
-import { RouteComponentProps } from '@reach/router';
-import { ServiceModal } from '../components/ServiceModal';
-import { IServicesBody } from '../../api/Interfaces';
-import { fetchServices, removeServices, saveServices, updateServices } from '../../api/Api';
+import { Button } from 'semantic-ui-react';
 import { DeleteConfirmationModal } from '../components/DeleteConfirmationModal';
-
-interface Iprops extends RouteComponentProps {
-}
-export const Services: React.FunctionComponent<Iprops> = (props: Iprops) => {
-
-	const [services, setServices] = useState<IServicesBody[]>([]);
+import { Table } from 'react-bootstrap';
+import { FaTrashAlt, FaEdit } from 'react-icons/fa';
+import { IServicesBody } from '../../api/Interfaces';
+import { fetchServices, removeServices, updateServices, saveServices } from '../../api/Api';
+import { ServiceModal } from '../components/ServiceModal';
+export const Services=()=>{
+  const [open, setOpen] = React.useState(false)
+  const toggle=()=> setOpen(!open);
+  
+  const [services, setServices] = useState<IServicesBody[]>([]);
 	const [deletedId, setDeletedId] = useState<number>();
 	const [editedService, setEditedSerive] = useState<IServicesBody>();
 
@@ -27,8 +26,8 @@ export const Services: React.FunctionComponent<Iprops> = (props: Iprops) => {
 		})
 		setDeletedId(undefined);
 		setEditedSerive(undefined);
-	}
-	const removeService = () => {
+  }
+  const removeService = () => {
 		deletedId && removeServices(deletedId).then((res) => {
 			console.log(res)
 			getData();
@@ -41,59 +40,91 @@ export const Services: React.FunctionComponent<Iprops> = (props: Iprops) => {
 			removeService()
 		}
 	}
-	const onServiceAdded = (addedService: IServicesBody) => {
-		let formData = new FormData();
-		formData.append('title', addedService.title || '');
-		formData.append('description', addedService.description || '');
-		if(!editedService) 
-			saveServices(formData).then((res) => { getData() }).catch((error) => { console.log(error); })
-		else{
-			(editedService.id) && 
-				updateServices(formData, editedService.id).then((res) => { getData() }).catch((error) => { console.log(error); })
-		}
-	}
-	return (
-		<div id="dashboard-services" >
-			<div className="container">
-				<h2>Services</h2>
-				<button className="btn btn-custom btn-lg" style={{ margin: '10px' }} data-toggle="modal" data-target="#myModal">
-					Add new Service
-        </button>
-				<ServiceModal onAdd={onServiceAdded} service={editedService}/>
-				<DeleteConfirmationModal deleteResponse={deleteResponse} label="Service" />
-				<Table hover size="sm">
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Description</th>
-							<th></th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{services?.map((service: IServicesBody, i: number) => {
-							return (
-								<tr key={i}>
-									<td>
-										{service.title}
-									</td>
-									<td>{service.description}</td>
-									<td>
-										<a data-toggle="modal" data-target="#myModal" onClick={() => { setEditedSerive(service) }}>
-											<FaEdit color="royalblue" />
-										</a>
-									</td>
-									<td>
-										<a data-toggle="modal" data-target="#confirm-delete" onClick={() => setDeletedId(service.id)}>
-											<FaTrashAlt color="coral" />
-										</a>
-									</td>
-								</tr>
-							)
-						})}
-					</tbody>
-				</Table>
-			</div>
-		</div>
-	)
+  
+  const onEdit = (service:IServicesBody)=>{
+    toggle();
+    setEditedSerive(service);
+  }
+  const onAction =(e:boolean)=>{
+    setOpen(e);
+    setEditedSerive(undefined);
+  }
+  const onSubmit=(newService:IServicesBody)=>{
+
+    let formData = new FormData();
+    formData.append('title', newService.title || '');
+		formData.append('description', newService.description || '');
+    if(editedService && editedService.id)  
+				updateServices(formData, editedService.id).then((res) => { afterSubmition() }).catch((error) => { console.log(error); })
+    else saveServices(formData).then((res) => { afterSubmition() }).catch((error) => { console.log(error); })
+
+    
+  }
+  const afterSubmition=()=>{
+    setOpen(false);
+    setEditedSerive(undefined);
+    getData();
+  }
+  return (
+    
+
+<div id="dashboard-services" >
+<div className="container">
+  <h2>Services</h2>
+  <>
+    <Button onClick={toggle} className="btn btn-custom btn-lg" style={{ 
+        fontFamily: 'Raleway',
+        textTransform: 'uppercase',
+        color: '#fff',
+        backgroundColor: '#c95f44',
+        backgroundImage: 'linear-gradient(to right, #db3630 0%, #c95f44 100%)',
+        padding: '14px 34px',
+        letterSpacing: '1px',
+        margin: '10',
+        fontSize: '15px',
+        fontWeight: '500',
+        borderRadius: '25px',
+        transition: 'all 0.5s linear',
+        border: '0',
+        width:'250px'
+     }}>Add Service</Button>
+    <ServiceModal open={open} onAction={(e:boolean)=>onAction(e)} service={editedService} onSubmit={onSubmit}/>
+    </>
+  {/* <ServiceModal onAdd={onServiceAdded} service={editedService} onClose={flushData}/> */}
+  <DeleteConfirmationModal deleteResponse={deleteResponse} label="Service" />
+  <Table hover size="sm">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Description</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {services?.map((service: IServicesBody, i: number) => {
+        return (
+          <tr key={i}>
+            <td>
+              {service.title}
+            </td>
+            <td>{service.description}</td>
+            <td>
+              <a data-toggle="modal" data-target="#myModal" data-backdrop="static" data-keyboard="false" onClick={()=>onEdit(service)}>
+                <FaEdit color="royalblue" />
+              </a>
+            </td>
+            <td>
+              <a data-toggle="modal" data-target="#confirm-delete" data-backdrop="static" data-keyboard="false" onClick={() => setDeletedId(service.id)}>
+                <FaTrashAlt color="coral" />
+              </a>
+            </td>
+          </tr>
+        )
+      })}
+    </tbody>
+  </Table>
+</div>
+</div>
+  );
 }
